2a2a157302845b02c222b57737c33f45ee53dc14
Jessica <jesscara.993@gmail.com>
2022-11-08 15:18:00 +0100

    myButton function tested

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ const nomeCognome = document.getElementById("name");
 const distanza = document.getElementById("km");
 const eta = document.getElementById("eta");
 const myButton = document.getElementById("genera");
+
+// BOTTONE GENERA
+myButton.addEventListener("click",
+  function(){
+    document.getElementById("biglietto").innerHTML = `Il tuo nome è ${nomeCognome.value} e devi percorrere ${distanza.value} km`;
+
+    nomeCognome.value = "";
+    distanza.value = "";
+  }
+)
