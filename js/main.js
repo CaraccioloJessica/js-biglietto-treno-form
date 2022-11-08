faa948731ed9d3b39b2af9d2037cef2b6f2b3b92
Jessica <jesscara.993@gmail.com>
2022-11-08 17:15:01 +0100

    condition defined (?)

--- js/main.js
+++ js/main.js
@@ -4,12 +4,31 @@ const distanza = document.getElementById("km");
 const eta = document.getElementById("eta");
 const myButton = document.getElementById("genera");
 
+let prezzo = distanza * 0.21;
+let offerta = 'Standard';
+let sconto;
+let prezzoFinale = prezzo;
+
 // BOTTONE GENERA
 myButton.addEventListener("click",
   function(){
-    document.getElementById("biglietto").innerHTML = `Il tuo nome è ${nomeCognome.value}, devi percorrere ${distanza.value} km e puoi usufruire di tariffa ${eta.value}.`;
+    if (eta === 'Giovani'){
+      offerta = 'Tariffa Giovani'
+      sconto = prezzo * 20 / 100;
+      prezzoFinale = prezzo - sconto;
+    }
 
-    nomeCognome.value = "";
-    distanza.value = "";
+    else if (eta == 'Senior') {
+      offerta = 'Sconto Over65'
+      sconto = prezzo * 40 / 100;
+      prezzoFinale = prezzo - sconto;
+    }
+
+
+
+
+
+
+    // document.getElementById("biglietto").innerHTML = `Il tuo nome è ${nomeCognome.value}, devi percorrere ${distanza.value} km e puoi usufruire di tariffa ${eta.value}.`;
   }
 )
