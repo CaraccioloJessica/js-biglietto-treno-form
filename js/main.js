5de54de5f897b2f4aac2538cd1f18d94a8ec7d2a
Jessica <jesscara.993@gmail.com>
2022-11-08 14:48:14 +0100

    standard const defined

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// VARIABILI
+const nomeCognome = document.getElementById("name");
+const distanza = document.getElementById("km");
+const eta = document.getElementById("eta");
+const myButton = document.getElementById("genera");
